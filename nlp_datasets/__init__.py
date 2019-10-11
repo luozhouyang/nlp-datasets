8bf516acf240efc9a7d1bc4f6e1aca2a68a32043
luozhouyang <zhouyang.luo@gmail.com>
2019-10-11 15:22:09 +0800

    Add tokenizers

--- nlp_datasets/__init__.py
+++ nlp_datasets/__init__.py
@@ -1,5 +1,7 @@
 from nlp_datasets.abstract_dataset import AbstractXYDataset, AbstractXYZDataset
 from nlp_datasets.tokenizers import AbstractTokenizer, SpaceTokenizer
+from nlp_datasets.tokenizers import TokenizerFilter
+from nlp_datasets.tokenizers import XYSpaceTokenizer, XYZSpaceTokenizer
 from nlp_datasets.xy_dataset import XYSameFileDataset, XYSeparateFileDataset
 from nlp_datasets.xyz_dataset import XYZSameFileDataset, XYZSeparateFileDataset
 
--- nlp_datasets/tokenizers/space_tokenizer.py
+++ nlp_datasets/tokenizers/space_tokenizer.py
@@ -27,3 +27,34 @@ class SpaceTokenizer(AbstractTokenizer):
             self._token2id_dict[w] = self._index
             self._id2token_dict[self._index] = w
             self._index += 1
+
+
+class XYSpaceTokenizer(SpaceTokenizer):
+
+    def _process_line(self, line, token_filters=None):
+        if not line:
+            return
+        seqs = line.split(self.config['line_sep'])  # [x, y] or [x, y, z]
+        for seq in seqs[0:2]:  # [x, y]
+            for w in seq.split(' '):
+                if w in self._special_tokens:
+                    continue
+                if w in self._token2id_dict:
+                    continue
+                if token_filters:
+                    if any(f.drop_token(w) for f in token_filters):
+                        continue
+                self._token2id_dict[w] = self._index
+                self._id2token_dict[self._index] = w
+                self._index += 1
+
+    def _get_default_config(self):
+        p = super(XYSpaceTokenizer, self)._get_default_config()
+        p.update({
+            "line_sep": '@@@'
+        })
+        return p
+
+
+class XYZSpaceTokenizer(XYSpaceTokenizer):
+    pass

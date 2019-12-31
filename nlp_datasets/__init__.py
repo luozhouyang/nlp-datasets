f7d1e671cc016dd0b8688aa0d0ef16ce20aa9e9e
luozhouyang <zhouyang.luo@gmail.com>
2019-12-31 14:57:43 +0800

    Organize import

--- nlp_datasets/__init__.py
+++ nlp_datasets/__init__.py
@@ -1,8 +1,10 @@
 from nlp_datasets.abstract_dataset import AbstractXYDataset, AbstractXYZDataset
+from nlp_datasets.seq2seq import Seq2SeqDataset
+from nlp_datasets.seq_classify import SeqClassifyDataset
+from nlp_datasets.seq_match import SeqMatchDataset
 from nlp_datasets.tokenizers import AbstractTokenizer, SpaceTokenizer
 from nlp_datasets.tokenizers import NumbersTokenFilter, RegexTokenFilter
 from nlp_datasets.tokenizers import TokenFilter, EmptyTokenFilter, LengthTokenFilter
-
 from nlp_datasets.xy_dataset import XYSameFileDataset, XYSeparateFileDataset
 from nlp_datasets.xyz_dataset import XYZSameFileDataset, XYZSeparateFileDataset
 
